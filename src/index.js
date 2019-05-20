ac406116a4ff0a31b048eb082ae0d18728507081
Felipe Oliveira <flpoliveira42@gmail.com>
2019-05-20 21:05:15 +0000

    Organizando as pastas para ficar facil encalonar

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import createCardElement from "./components/memory-card.js";
-
-createCardElement(1, "-back", "icon-js.png");
-createCardElement(5);
---
+++ src/pages/game.js
@@ -0,0 +1,4 @@
+import createCardElement from "../components/memory-card/memory-card.js";
+
+createCardElement(1, "-back", "icon-js.png");
+createCardElement(5);
