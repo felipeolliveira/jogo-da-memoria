ae990ea38704324024b80d04ad1ce979696fa555
Felipe Oliveira <flpoliveira42@gmail.com>
2019-05-20 16:20:14 +0000

    Alteração do inner html para insertAdjacentHTML

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
+const $icon = `<img
+    class="icon"
+    src="../img/icon-collabcode.svg"
+    alt="Icone da COllabCode, mascote Gueio"
+/>`;
+
+$root.insertBefore($memoryCard, null);
 
 $memoryCard.classList.add("memory-card");
-$root.insertBefore($memoryCard, null);
+$memoryCard.insertAdjacentHTML("beforeend", $icon);
