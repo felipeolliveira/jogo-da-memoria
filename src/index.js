887a6a22122a0547690a50fe8a838eae816fdbf1
Felipe Oliveira <flpoliveira42@gmail.com>
2019-05-20 18:10:08 +0000

    Refatorando o nome das variáveis

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 const $root = document.querySelector("#root");
 
 const $memoryCard = document.createElement("article");
-const $memoryCard2 = document.createElement("article");
+const $memoryCardBack = document.createElement("article");
 
-const $icon = `<img
+const $iconCollab = `<img
   class="icon"
   src="../img/icon-collabcode.svg"
   alt="Icone da COllabCode, mascote Gueio"
 />`;
 
-const $icon2 = `<img
+const $iconJS = `<img
   class="icon"
   src="./img/-icon-js.png"
   alt="Icone de Javascript"
 />`;
 
 $memoryCard.classList.add("memory-card");
-$memoryCard2.classList.add("memory-card", "-back");
+$memoryCardBack.classList.add("memory-card", "-back");
 
-$memoryCard.insertAdjacentHTML("beforeend", $icon);
-$memoryCard2.insertAdjacentHTML("beforeend", $icon2);
+$memoryCard.insertAdjacentHTML("beforeend", $iconCollab);
+$memoryCardBack.insertAdjacentHTML("beforeend", $iconJS);
 
-$root.insertBefore($memoryCard2, null);
+$root.insertBefore($memoryCardBack, null);
 $root.insertBefore($memoryCard, null);
