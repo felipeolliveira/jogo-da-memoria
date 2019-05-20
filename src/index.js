266723d1a228baf4cbb474e15dd575fd2e416802
Felipe Oliveira <flpoliveira42@gmail.com>
2019-05-20 15:58:43 +0000

    Adicionando um index.js para inserir elementos na div #root

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const $root = document.querySelector("#root");
+const $memoryCard = document.createElement("article");
+
+$memoryCard.classList.add("memory-card");
+$root.insertBefore($memoryCard, null);
