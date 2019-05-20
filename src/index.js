ba278ac4f6913ef3370ed80bb514fe874e7cb8c1
Felipe Oliveira <flpoliveira42@gmail.com>
2019-05-20 16:52:59 +0000

    Aula 21 - Todos os desafios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 const $root = document.querySelector("#root");
+
 const $memoryCard = document.createElement("article");
+const $memoryCard2 = document.createElement("article");
+
 const $icon = `<img
-    class="icon"
-    src="../img/icon-collabcode.svg"
-    alt="Icone da COllabCode, mascote Gueio"
+  class="icon"
+  src="../img/icon-collabcode.svg"
+  alt="Icone da COllabCode, mascote Gueio"
 />`;
 
-$root.insertBefore($memoryCard, null);
+const $icon2 = `<img
+  class="icon"
+  src="./img/-icon-js.png"
+  alt="Icone de Javascript"
+/>`;
 
 $memoryCard.classList.add("memory-card");
+$memoryCard2.classList.add("memory-card", "-back");
+
 $memoryCard.insertAdjacentHTML("beforeend", $icon);
+$memoryCard2.insertAdjacentHTML("beforeend", $icon2);
+
+$root.insertBefore($memoryCard2, null);
+$root.insertBefore($memoryCard, null);
