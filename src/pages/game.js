9e40ffc89c153ef94476322fd461dce69ed20425
Felipe Oliveira <flpoliveira42@gmail.com>
2019-05-24 14:33:31 -0300

    Aula 25 - Desafio Básico/intermediário

--- src/pages/game.js
+++ src/pages/game.js
@@ -4,24 +4,34 @@ import { createCard } from "../components/memory-card/index.js";
 const $root = document.querySelector("#root");
 
 const $cardsWrapper = createcardsWrapper();
-const $memoryCardBack = createCard(
-  "img/icon-js.png",
-  "Icone de Javascript",
-  "-back"
-);
-const $memoryCardFront = createCard(
+const $GueioCard = createCard(
   "img/icon-collabcode.svg",
   "Icone do Gueio, mascote da collabcode",
   ""
 );
+const $JavaScriptCard = createCard(
+  "img/icon-js.png",
+  "Icone da linguagem Javascript",
+  "-back"
+);
+const $CppCard = createCard(
+  "img/icon-c.png",
+  "Icone da linguagem C++",
+  "-back"
+);
+const $bugCard = createCard(
+  "img/icon-bug.png",
+  "Icone do Gueio, mascote da collabcode",
+  "-back"
+);
 
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBack);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBack);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBack);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBack);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBack);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardBack);
+$cardsWrapper.insertAdjacentHTML("beforeend", $GueioCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $GueioCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $JavaScriptCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $JavaScriptCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $CppCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $CppCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $bugCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $bugCard);
 
 $root.insertAdjacentElement("beforeend", $cardsWrapper);
