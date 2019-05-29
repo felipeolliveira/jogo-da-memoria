10fdd87712858d8cb510c72a98cb194f4f552e2e
Felipe Oliveira <flpoliveira42@gmail.com>
2019-05-29 18:07:32 -0300

    Adicionando o css dentro do javascript do componente. Inserindo o css em uma tag style dentro do head como ultimo filho. Feito um if ternário para não ter undefined dentro das classes vazias

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,5 +1,5 @@
 const createCard = ({ className, src, alt }) => `
-  <article class="memory-card ${className}">
+  <article class="memory-card ${className === undefined ? "" : className}">
     <img
       class="icon"
       src= ${src}
