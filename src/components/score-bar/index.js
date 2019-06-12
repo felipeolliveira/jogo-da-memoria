6cbf40eb2eebc61cce68d512257bbfdc94a48d66
Felipe Oliveira <flpoliveira42@gmail.com>
2019-06-12 15:28:49 -0300

    Refatorado o component score-bar

--- src/components/score-bar/index.js
+++ src/components/score-bar/index.js
@@ -1,16 +1,16 @@
 const scoreBar = (function() {
   const module = {};
 
-  module.create = () => {
+  module._style = function() {
     const $style = document.createElement("style");
     $style.textContent = `
     .score-bar {
-      width: 50px;
+      width: 200px;
       height: 50px;
       margin: 0 auto;
       background-color: #f25a70;
       margin-bottom: 20px;
-      border-radius: 0 0 25px 25px;
+      border-radius: 0 0 10px 10px;
       display: flex;
       justify-content: center;
       align-items: center;
@@ -21,8 +21,12 @@ const scoreBar = (function() {
       font-weight: bold;
       color: #fff;
     }
-  `;
+    `;
     document.head.insertAdjacentElement("beforeend", $style);
+  };
+
+  module.create = () => {
+    module._style();
 
     return `
     <header class="score-bar">
