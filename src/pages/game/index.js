d008042c81f5d202ba5d6235bbcb0af4fe366346
Felipe Oliveira (Windows) <flpoliveira42@gmail.com>
2019-06-25 14:29:30 -0300

    Adicionando uma IIFE no game

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,66 +1,68 @@
-const $root = document.querySelector("#root");
+(function() {
+  const $root = document.querySelector("#root");
 
-const $scoreBar = scoreBar.create();
-const $gameWrapper = gameWrapper.create();
-const $layerStart = layerStart.render();
-const createCard = memoryCard.create();
+  const $scoreBar = scoreBar.create();
+  const $gameWrapper = gameWrapper.create();
+  const $layerStart = layerStart.render();
+  const createCard = memoryCard.create();
 
-const $javascriptCard = createCard({
-  src: "img/icon-js.png",
-  alt: "Icone da linguagem Javascript"
-});
-const $cppCard = createCard({
-  src: "img/icon-c.png",
-  alt: "Icone da linguagem C++"
-});
-const $bugCard = createCard({
-  src: "img/icon-bug.png",
-  alt: "Icone do inseto - o bug usado em informática"
-});
-const $womanCard = createCard({
-  src: "img/icon-woman.png",
-  alt: "Icone de uma mulher usando um laptop"
-});
-
-const newRandomCards = () => {
-  $gameWrapper.innerHTML = "";
-  const selectCards = [$javascriptCard, $cppCard, $bugCard, $womanCard];
-
-  const duplicateCards = () => {
-    const cards = [];
-    for (let i = 0; i < 2; i++) {
-      cards.push(...selectCards);
-    }
-    return cards;
-  };
-
-  const cardsShuffler = () => {
-    let cards = duplicateCards();
-    let finalCards = [];
-
-    while (cards.length) {
-      const randomNumber = Math.floor(Math.random() * cards.length);
-      const randomCard = cards[randomNumber];
-      const indexCard = cards.indexOf(randomCard);
-
-      if (indexCard > -1) {
-        finalCards.push(randomCard);
-        cards.splice(indexCard, 1);
-      }
-    }
-
-    return finalCards;
-  };
-
-  const newCards = cardsShuffler();
-
-  newCards.forEach(card => {
-    $gameWrapper.insertAdjacentHTML("beforeend", card);
+  const $javascriptCard = createCard({
+    src: "img/icon-js.png",
+    alt: "Icone da linguagem Javascript"
   });
-};
+  const $cppCard = createCard({
+    src: "img/icon-c.png",
+    alt: "Icone da linguagem C++"
+  });
+  const $bugCard = createCard({
+    src: "img/icon-bug.png",
+    alt: "Icone do inseto - o bug usado em informática"
+  });
+  const $womanCard = createCard({
+    src: "img/icon-woman.png",
+    alt: "Icone de uma mulher usando um laptop"
+  });
 
-newRandomCards();
+  const newRandomCards = () => {
+    $gameWrapper.innerHTML = "";
+    const selectCards = [$javascriptCard, $cppCard, $bugCard, $womanCard];
 
-$root.insertAdjacentHTML("beforeend", $layerStart);
-$root.insertAdjacentHTML("beforeend", $scoreBar);
-$root.insertAdjacentElement("beforeend", $gameWrapper);
+    const duplicateCards = () => {
+      const cards = [];
+      for (let i = 0; i < 2; i++) {
+        cards.push(...selectCards);
+      }
+      return cards;
+    };
+
+    const cardsShuffler = () => {
+      let cards = duplicateCards();
+      let finalCards = [];
+
+      while (cards.length) {
+        const randomNumber = Math.floor(Math.random() * cards.length);
+        const randomCard = cards[randomNumber];
+        const indexCard = cards.indexOf(randomCard);
+
+        if (indexCard > -1) {
+          finalCards.push(randomCard);
+          cards.splice(indexCard, 1);
+        }
+      }
+
+      return finalCards;
+    };
+
+    const newCards = cardsShuffler();
+
+    newCards.forEach(card => {
+      $gameWrapper.insertAdjacentHTML("beforeend", card);
+    });
+  };
+
+  newRandomCards();
+
+  $root.insertAdjacentHTML("beforeend", $layerStart);
+  $root.insertAdjacentHTML("beforeend", $scoreBar);
+  $root.insertAdjacentElement("beforeend", $gameWrapper);
+})();
